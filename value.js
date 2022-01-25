c5c6442ba070d429393fa060ec23691ed30da443
Igor' Golub <greengadzila@gmail.com>
2022-01-25 22:56:55 +0300

    add file

---
+++ value.js
@@ -0,0 +1,11 @@
+function evalObject(value){
+    var result = 0;
+    switch(value.operation){
+      case '+': return  value.a + value.b;
+      case '-': return  value.a - value.b;
+      case '/': return  value.a / value.b;
+      case '*': return  value.a * value.b;
+      case '%': return  value.a % value.b;
+      case '^': return  Math.pow(value.a, value.b);
+    }
+  }
