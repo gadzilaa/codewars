492548546f723ea0bd1cb234a47e2c7a7776c17b
Igor' Golub <greengadzila@gmail.com>
2022-01-26 18:10:03 +0300

    add file

---
+++ If/ Grasshopper - Debug.js
@@ -0,0 +1,14 @@
+function weatherInfo (temp) {
+    let c = convertToCelsius(temp);
+    if (c > 0){
+      return (c + " is above freezing temperature");
+    }else{
+      return (c + " is freezing temperature");
+  }
+  }
+    
+  
+  function convertToCelsius (temperature) {
+    var celsius = (temperature - 32) * (5/9);
+    return celsius;
+  }
