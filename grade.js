ebc8fb35afb497f18c1c0f65008c05bcd41f93fa
Igor' Golub <greengadzila@gmail.com>
2022-01-26 13:38:55 +0300

    add file

---
+++ grade.js
@@ -0,0 +1,14 @@
+function getGrade (s1, s2, s3) {
+    let result = (s1+s2+s3)/3;
+    if (result >= 90 && result <= 100){
+      return "A";
+    } else if (result>=80){
+      return "B";
+    } else if (result >= 70){
+      return "C";
+    } else if (result >= 60){
+      return "D";
+    } else {
+      return "F"
+    }
+  }
